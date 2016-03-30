2cbfe660e4497cdd19057a32821488a331c7fd9f
matmax <matthias.graf@st.ovgu.de>
2016-03-30 12:20:11 +0200

    circular arrangement, click on image to center, shift according to similarity

--- js/main.js
+++ js/main.js
@@ -24,9 +24,12 @@ var jsons = [
 var resolutions = ["area10000", "area100000", "area1000000"]
 
 withLoadedJSONfiles(jsons, function([fileList, similarity, created]) {
-	console.log(fileList)
-	console.log(created)
-	created.forEach(function(e) {
+	// indices are the same across input arrays!
+	// retain indices for later referencing (after sort reordered array)
+	var images = fileList.map((e,i) => ({file: e, index: i}))
+	
+	
+	created = created.forEach(function(e, i) {
 		if (e) { // some images may not have EXIF data, e.g. panoramas
 			// "2008:10:11 16:42:31"
 			var match = /^(\d{4}):(\d{2}):(\d{2}) (\d{2}):(\d{2}):(\d{2})/.exec(e)
@@ -39,11 +42,12 @@ withLoadedJSONfiles(jsons, function([fileList, similarity, created]) {
 				Number(match[5]),
 				Number(match[6])
 			)
-			// TODO do something with this
-			console.log(e, date)
+			images[i].date = date
+		} else {
+			images[i].date = undefined
 		}
 	})
-	
+	//console.log(created)
 	
 	// similarity[i][j] refers to fileList[i] & fileList[j]
 	// high value is low similarity
@@ -52,18 +56,42 @@ withLoadedJSONfiles(jsons, function([fileList, similarity, created]) {
 	// normalise into [0,1]
 	var max = Math.max(...similarity.map(arr => Math.max(...arr)))
 	similarity = similarity.map(arr => arr.map(e => e/max))
+	
+	similarity.forEach((arr,i) => images[i].similarity = arr)
 	var flatSim = [].concat(...similarity)
 	
+	images.sort((a,b) => {
+		// compare by date, fall back to compare by name if no date available
+		if (!a.date || !b.date)
+			return a.file < b.file ? -1 : 1
+		return a.date < b.date ? -1 : 1
+	})
+	
+	images = images.slice(0, 20)
 	
 	//createHistogram(flatSim)
-	
-	pixi(fileList)
+	pixi(images)
 })
 
 
+function polarCoordinates(w, h, alpha, r) {
+	var center = {x: w/2, y: h/2}
+	var x = Math.cos(alpha)*r
+	var y = Math.sin(alpha)*r
+	var p = {x: center.x + x, y: center.y + y} 
+	return p
+}
 
-function pixi(fileList) {
-	var w = 2100, h = 1200
+function positionImage(w, h, image) {
+	var p = polarCoordinates(w, h, image.alpha, image.r)
+	image.sprite.position.x = p.x
+	image.sprite.position.y = p.y
+}
+
+
+
+function pixi(images) {
+	var w = 1500, h = 800
 	var renderer = new PIXI.WebGLRenderer(w, h)
 	document.body.appendChild(renderer.view)
 	var stage = new PIXI.Container()
@@ -71,33 +99,73 @@ function pixi(fileList) {
 	// The ParticleContainer class is a really fast version of the Container built solely for speed, so use when you need a lot of sprites or particles. The tradeoff of the ParticleContainer is that advanced functionality will not work. ParticleContainer implements only the basic object transform (position, scale, rotation). Any other functionality like tinting, masking, etc will not work on sprites in this batch.
 	var container = new PIXI.ParticleContainer()
 	stage.addChild(container)
-	allImages = []
 	
-	function loadImageInPixi(path) {
-		var sprite = new PIXI.Sprite.fromImage(path)
-		sprite.position.x = Math.random()*w
-		sprite.position.y = Math.random()*h
-		//sprite.scale.x = 0.5
-		//sprite.scale.y = 0.5
-		allImages.push(sprite)
-		//container.addChild(sprite)
+	var alpha = 0
+	
+	function loadImageInPixi(image) {
+		var sprite = new PIXI.Sprite.fromImage("images/area10000/"+image.file)
+		image.sprite = sprite
+		image.alpha = alpha
+		image.r = Math.min(w,h)*0.3
+		
+		// alpha = 0 is EAST
+		//var p = polarCoordinates(w, h, image.alpha, image.r)
+		positionImage(w, h, image)
+		alpha += 1/images.length*2*Math.PI
+		sprite.interactive = true
+		// turns pointer to hand on mouseover
+		//sprite.buttonMode = true
+		
+		var onButtonDown = function(mouseData) {
+			centerImage(image)
+			image.sprite.scale.x += 0.3
+			image.sprite.scale.y += 0.3
+		}
+		
+		sprite.on('mousedown', onButtonDown)
+		
+		sprite.anchor.x = 0.5
+		sprite.anchor.y = 0.5
+		sprite.scale.x = 0.7
+		sprite.scale.y = 0.7
 		stage.addChild(sprite)
+	}
+	
+	function centerImage(image) {
+		images.forEach(img => {
+			var similarity = image.similarity[img.index]
+			
+			img.r = Math.min(w,h)*0.2 + similarity*Math.min(w,h)*1.2
+			
+			img.sprite.scale.x = 0.7 - similarity
+			img.sprite.scale.y = 0.7 - similarity
+			
+			positionImage(w, h, img)
+		})
+		
+		image.sprite.scale.x = 1
+		image.sprite.scale.y = 1
+		image.r = 0
+		positionImage(w, h, image)
 	}
 	
 	// firefox: about:config: layers.acceleration.draw-fps
 	// ~40 fps with 2260 images
 	// ~10 fps with 22600 images
-	new Array(20).fill(0).forEach(() => 
-		fileList.forEach(file => loadImageInPixi("images/area1000/"+file))
+	new Array(1).fill(0).forEach(() =>
+		images.forEach(e => loadImageInPixi(e))
 	)
 	
+	centerImage(images[0])
+	
+		
 	function animate() {
 		// start the timer for the next animation loop
 		requestAnimationFrame(animate)
 		
 		// rotation not slower than moving
 		// each frame we spin the image around a bit
-		allImages.forEach(img => img.rotation += Math.random()/100)
+		//images.forEach(img => img.sprite.rotation += Math.random()/100)
 		
 		
 		// this is the main render call that makes pixi draw your container and its children.
